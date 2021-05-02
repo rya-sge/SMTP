ed6bc114fac3bf91010ac9372b77cb1766325be7
rya-sge <ryan.sauge@heig-vd.ch>
2021-05-02 02:33:56 +0200

    renommer fichier

--- docker/build-image-basique.sh
+++ docker/build-image-basique.sh
@@ -3,9 +3,5 @@
 # Ask maven to build the executable jar file from the source files
 mvn clean install --file ../pom.xml
 
-# Copy the executable jar file in the current directory
-#If you want build from sources
-cp ../target/MockMock-1.4.0.one-jar.jar .
-
 # Build the Docker image locally
 docker build --tag smtp_sauge_viotti/smtp .
